df62ba29389dc9957f1c6554a7b09c60755b3198
Hao Guo <guo0012@hz.nl>
2021-05-28 22:17:56 +0200

    login after register

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -21,15 +21,15 @@ class AuthController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8|confirmed',
-            'password_confirmation' => 'required',
         ]);
 
-        User::create([
+        $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
 
+        Auth::login($user);
         return redirect('home');
     }
 
